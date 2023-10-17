52f603dad52cc1a37e8eb9aec6c28ebf3e87b1d1
paulavsmt <pasanmartin2021@udec.cl>
2023-10-16 21:21:13 -0300

    Create Moneda.java

---
+++ Moneda.java
@@ -0,0 +1,43 @@
+abstract class Moneda{
+    public Moneda(){
+    }
+    public Moneda getSerie(){
+        return this;
+    }
+    public abstract int getValor();
+}
+
+class Moneda100 extends Moneda{
+    public Moneda100(){
+        super();
+    }
+    public int getValor(){
+        return 100;
+    }
+}
+
+class Moneda500 extends Moneda{
+    public Moneda500(){
+        super();
+    }
+    public int getValor(){
+        return 500;
+    }
+}
+
+class Moneda1000 extends Moneda{
+    public Moneda1000(){
+        super();
+    }
+    public int getValor(){
+        return 1000;
+    }
+}
+class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+    }
+    public int getValor(){
+        return 1500;
+    }
+}
