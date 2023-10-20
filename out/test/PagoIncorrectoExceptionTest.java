700b57ad92cc18bc8eec73b702cc2847d648ebad
paulavsmt <pasanmartin2021@udec.cl>
2023-10-20 19:21:32 -0300

    Update PagoIncorrectoExceptionTest.java

--- out/test/PagoIncorrectoExceptionTest.java
+++ out/test/PagoIncorrectoExceptionTest.java
@@ -6,20 +6,13 @@ class PagoIncorrectoExceptionTest {
     // caso, espendedor valido y..
     //moneda nula, debe devolver la excepcion PagoIncorrectoException
    @Test
-   void pr() {
+   void pr() throws PagoIncorrectoException {
        Expendedor exp = new Expendedor(1);
        Moneda m =null;
-       try {
+       Comprador c = new Comprador(m, 1, exp);
+       Producto cc = exp.comprarProducto(m, 1);
+       System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
 
-           Comprador c = new Comprador(m, 1, exp);
-           Producto cc = exp.comprarProducto(m, 1);
-           System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
-
-       }
-       catch (PagoIncorrectoException w){
-           System.out.println("Error: "+w.getMessage());
-
-       }
 
    }
 }
