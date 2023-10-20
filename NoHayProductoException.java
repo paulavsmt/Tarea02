d9c12196c98ddbae9484c2b40814c29a180d0adf
paulavsmt <pasanmartin2021@udec.cl>
2023-10-20 14:47:37 -0300

    Create NoHayProductoException.java

---
+++ NoHayProductoException.java
@@ -0,0 +1,10 @@
+public class NoHayProductoException extends Exception {
+
+    public NoHayProductoException() {
+        super("Excepcion al seleccionar un producto que no hay/queda");
+    }
+
+    public NoHayProductoException(String mensaje) {
+        super(mensaje);
+    }
+}
