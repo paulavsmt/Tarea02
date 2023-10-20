c857885f7ba0141520ac7cafe11546e426dfc291
paulavsmt <pasanmartin2021@udec.cl>
2023-10-20 18:27:56 -0300

    Update PagoIncorrectoExceptionTest.java

--- out/test/PagoIncorrectoExceptionTest.java
+++ out/test/PagoIncorrectoExceptionTest.java
@@ -11,22 +11,8 @@ class PagoIncorrectoExceptionTest {
         Expendedor exp = new Expendedor(1);
         Moneda m = null;
         Comprador c=null;
-        m = null;
         c = new Comprador(m, 1, exp);
-        exp.comprarProducto(,2)
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+       Producto cc= exp.comprarProducto(m, 1)
+       System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
     }
 }
-
-// o sea, tengo esto aca por que habia que definir las excepciones con su mensaje, no [ara los test
-// no se si se entiende(?)
-// jhhjjhjhjhf que entretenido hablar asi
-// dhfkjsdhfjk si
-// ni idea, lo estamos viendo, pero van en el main los try y los que gatillan las excepciones(trow) en el
-// metodo comprarProducto
-//
-
-
-
-// error mio, crei que estabas el el .java, no en el test, como tengo varias class abiertas no vi la extension
-// siga con lo suyo xD
