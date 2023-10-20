c4c2a0e70d5f5002bba49e617c604e3f0cd1653c
IgnCan <igcandia2017@udec.cl>
2023-10-20 10:11:03 -0300

    Update MainEjecucion.java

--- MainEjecucion.java
+++ MainEjecucion.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+// hare pruebas de ejecucion aca en lo que aprendo junit
 public class MainEjecucion {
+    public static void main(String[] args) {
+//        Expendedor exp = new Expendedor(3,500);
+//        Moneda m = null;
+//        Comprador c=null;
+//        m = new Moneda1500();
+//        c = new Comprador(m,Expendedor.SPRITE,exp);
+//        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+//        m = new Moneda1500();
+//        c = new Comprador(m,Expendedor.SPRITE,exp);
+//        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+//        m = new Moneda1500();
+//        c = new Comprador(m,Expendedor.SPRITE,exp);
+//        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        // caso, espendedor valido y..
+        Expendedor exp = new Expendedor(2,100);
+        Moneda m = null;
+        Comprador c=null;
+
+        //moneda nula, debe devolver la excepcion PagoIncorrectoException
+        m = null;
+        c = new Comprador(m,Expendedor.SPRITE,exp);
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        m = new Moneda500();
+        c = new Comprador(m,Expendedor.SPRITE,exp);
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        m = new Moneda1000();
+        c = new Comprador(m,Expendedor.SPRITE,exp);
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        m = new Moneda1000();
+        c = new Comprador(m,Expendedor.SPRITE,exp);
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+
+        m = new Moneda1000();
+        c = new Comprador(m,Expendedor.SUPER8,exp);
+        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
+    }
 }
