1806fdf4494112e15e3cb264e55f52a3ff7b09bb
paulavsmt <pasanmartin2021@udec.cl>
2023-10-20 14:21:46 -0300

    Create DepositoTest.java

---
+++ out/test/DepositoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositoTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void chequear() {
+    }
+}
