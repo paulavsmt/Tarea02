97fbb43eace3090f327c405a2f74d2b15d643959
paulavsmt <pasanmartin2021@udec.cl>
2023-10-20 14:43:49 -0300

    Update ExpendedorTest.java

--- out/test/ExpendedorTest.java
+++ out/test/ExpendedorTest.java
@@ -4,8 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExpendedorTest {
 
+    // caso, espendedor valido y..
+    Expendedor exp = new Expendedor(1);
+    Moneda m = null;
+    Comprador c=null;
     @Test
     void comprarProducto() {
+
     }
     @Test
     void getVuelto() {
